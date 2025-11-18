a054a1cc66a7ec2989e538cde2a4a5a4a6ef2a8b
developerAyesha <ayeshaakh234@gmail.com>
2025-11-18 10:30:55 +0500

    setUp the docker access token

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -5,4 +5,5 @@ const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || "https://developer-tag-backend-wz59.vercel.app/api/v1" // Adjust the base URL if necessary
 
 });
+
 export default API;
