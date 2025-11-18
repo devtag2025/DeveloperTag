9b10ff7f830fed6ab873109809febfef61f153df
developerAyesha <ayeshaakh234@gmail.com>
2025-11-18 10:13:17 +0500

    fixed the axios file

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -5,6 +5,4 @@ const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || "https://developer-tag-backend-wz59.vercel.app/api/v1" // Adjust the base URL if necessary
 
 });
-
-
 export default API;
