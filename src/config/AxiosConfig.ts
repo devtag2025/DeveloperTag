9d038a077a959e8182920738ef33e7654dfd9003
developerAyesha <ayeshaakh234@gmail.com>
2025-11-18 13:28:10 +0500

    fix the cors issue

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const API = axios.create({
-    baseURL: "https:/api.developertag.com//api/v1" // Adjust the base URL if necessary
+    baseURL: "https://api.developertag.com/api/v1" // Adjust the base URL if necessary
 
 });
 
